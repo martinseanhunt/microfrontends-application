f22c233253236b0d51c710323f29a861e89d36a5
Martin Hunt <martinseanhunt@gmail.com>
2021-02-04 15:05:52 +0000

    trigger build

--- container/src/mount.js
+++ container/src/mount.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom'
 import { App } from './App'
 
 // We don't need a mount function like in other FE's because the
-// container is never being run from within another FE
+// container is never being run from within another FE.
 ReactDOM.render(<App />, document.querySelector('#root'))
