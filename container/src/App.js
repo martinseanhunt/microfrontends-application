ec3225fe79df4250dd54c7656eab78a395384ae4
Martin Hunt <martinseanhunt@gmail.com>
2021-02-06 10:19:56 +0000

    fix production styling class conflicts

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 
 import { Header } from './components/Header'
 import { MarketingApp } from './components/remote/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+  // Prefixing all minified classnames with a string unique to this micro FE to avoid class naem collisoins.
+  productionPrefix: 'ct',
+})
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <>
+      {/* 
+      Giving styles provider an extra option  to make sure that classnames are sufficently random
+      when minified for production. This fixes an issue where there can be style name conflicts across
+      different micro FE's in prod. See https://www.udemy.com/course/microfrontend-course/learn/lecture/23207132#content
+      for more info.
+    */}
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Header />
+          <MarketingApp />
+        </BrowserRouter>
+      </StylesProvider>
+    </>
   )
 }
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 
 import { Landing } from './components/Landing'
 import { Pricing } from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+  // Prefixing all minified classnames with a string unique to this micro FE to avoid class naem collisoins.
+  productionPrefix: 'ma',
+})
+
 export const App = () => (
   <div>
-    <StylesProvider>
+    {/* 
+      Giving styles provider an extra option  to make sure that classnames are sufficently random
+      when minified for production. This fixes an issue where there can be style name conflicts across
+      different micro FE's in prod. See https://www.udemy.com/course/microfrontend-course/learn/lecture/23207132#content
+      for more info.
+    */}
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
